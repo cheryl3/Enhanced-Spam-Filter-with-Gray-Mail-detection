6ef5a7f1a18da84add5e1579b2344dfceded74ab
cheryl3 <chermer01@gmail.com>
2017-04-27 18:10:57 +0530

    Add files via upload

---
+++ Detect.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author cheryl
+ */
+public class Detect {
+    
+    int spamCount = 0;
+    int hamCount = 0;
+    Map<String, Node> map = new HashMap<String, Node>();
+    List<String> commonWords = new ArrayList<String>();
+    
+    public void init() throws Exception {
+	BufferedReader br = new BufferedReader( new FileReader( new File("C:\\Users\\cheryl\\Documents\\NetBeansProjects\\SpamDetector_v1\\src\\spamdetector_v1\\common-words.txt")));
+	String line = br.readLine();
+	while(line != null) {
+            commonWords.add(line);
+            line = br.readLine();
+	}
+	String spam_dataset = "C:\\Users\\cheryl\\Downloads\\dataset\\spam\\";
+        
+        File spamFolder = new File(spam_dataset);
+        File[] spamList = spamFolder.listFiles();
+                
+        for(File file : spamList){
+            br = new BufferedReader(new FileReader(file));
+            line = br.readLine();
+            while(line != null) {
+                line = line.toLowerCase();
+		String[] words = line.split(" ");
+		for( String s: words ) {
+                    if(s.length() > 3 && !commonWords.contains(s)) {
+			spamCount++;
+			if( map.containsKey(s)) {
+                            map.get(s).spamCount++;
+			} 
+                        else {
+                            map.put(s, new Node( 1, 0));
+			}
+                    }
+		}
+		line = br.readLine();
+            }
+        }
+                
+                
+                
+        String ham_dataset = "C:\\Users\\cheryl\\Downloads\\dataset\\ham\\";
+        
+        File hamFolder = new File(ham_dataset);
+        File[] hamList = hamFolder.listFiles();
+                
+        for(File file : hamList){
+            br = new BufferedReader(new FileReader(file));
+            line = br.readLine();
+            while(line != null) {
+		line = line.toLowerCase();
+		String[] words = line.split(" ");
+		for( String s: words ) {
+                    if(s.length() > 3 && !commonWords.contains(s)) {
+			hamCount++;
+			if( map.containsKey(s)) {
+                            map.get(s).hamCount++;
+			} 
+                        else {
+                            map.put(s, new Node( 0, 1));
+			}
+                    }
+		}
+		line = br.readLine();
+            }
+		
+        }
+                
+                
+		
+	Set<String> keys = map.keySet();
+	for( String key: keys ) {
+            Node node = map.get(key);
+            double res = ((node.spamCount)/(double)(spamCount))/(double)(((node.spamCount)/(double)(spamCount)) + (node.hamCount)/(double)(hamCount));
+            node.probability = res;
+	}
+		
+	br.close();
+    }
+    
+    
+    public void detect(String s) {
+
+        boolean result = false;
+	s = s.toLowerCase();
+	String[] sArr = s.split(" ");
+	TreeMap<Double, List<Double>> interestMap = new TreeMap<Double, List<Double>>(Collections.reverseOrder());
+        
+	for( String x: sArr ) {
+            
+	    if(x.length()> 3 && !commonWords.contains(x)) {
+		double i = 0.5;
+		double p = 0.5;
+		if(map.containsKey(x)) {
+                    p = map.get(x).probability;					
+		} 
+		
+                i = Math.abs(i - p);
+		if( !interestMap.containsKey(i) ) {
+                    List<Double> values = new ArrayList<Double>();
+                    values.add(p);						
+                    interestMap.put(i, values);
+		} 
+                else {
+                    interestMap.get(i).add(p);
+		}
+            }
+	}
+		
+	List<Double> probabilities = new ArrayList<Double>();
+	int count = 0;
+	Set<Double> set = interestMap.keySet();
+	for( Double d: set ) {
+            List<Double> list = interestMap.get(d);
+	
+            for(Double x: list) {
+		count++;
+		probabilities.add(x);
+		if(count == 15) {
+                	break;
+		}
+            }
+            if(count == 15) {
+		break;
+            }
+	}
+		
+		double res = 1;
+		double numerator = 1;
+		double denominator = 1;
+		for( Double d: probabilities ) {
+			numerator = numerator * d;
+			denominator = denominator * (1- d);
+		}
+		res = numerator/(double)(numerator +denominator);
+                
+		if(res >= 0.9) {
+			result = true;
+		}
+		
+                if( result ) {
+                    
+                    ScoreGrayCalculation adjoint = new ScoreGrayCalculation();
+                    
+                    ArrayList<String> tokens = adjoint.tokenize(s);
+                    int detect = adjoint.test(tokens, result);
+                    if(detect == 1)
+                        System.out.println("'" +s+ "' \n spam");
+                    if(detect == 2)
+                        System.out.println("'" +s+ "'\n gray");
+                    
+                        
+		} 
+                else{
+                    System.out.println("'" +s+ "'\n is not a spam");
+                }
+                        
+            	
+    }
+    
+    
+    public static void main(String args[]) throws Exception{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Detect winnow1 = new Detect();
+	winnow1.init();
+        
+        String s = null;
+                do{
+                    System.out.println("Enter your message(press 0 to stop)");
+                    StringBuilder everything = new StringBuilder();
+                    String line;
+                    while( !(line = br.readLine()).isEmpty()) {
+                        everything.append(line);
+                    }
+                    s = everything.toString();
+                    
+                    winnow1.detect(s);
+                }while(!"0".equals(s));
+                
+    }
+
+    
+}
+
+class Node {
+	int spamCount;
+	int hamCount;
+	double probability;
+	
+	public Node( int spamCount, int hamCount ) {
+		this.spamCount = spamCount;
+		this.hamCount = hamCount;
+	}
+	
+	public String toString() {
+		return String.valueOf(probability);
+	}
+}
---
+++ ScoreGrayCalculation.java
@@ -0,0 +1,376 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team;
+
+/**
+ *
+ * @author cheryl
+ */
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.Random;
+import java.util.TreeMap;
+
+
+public class ScoreGrayCalculation {
+
+    private static final String SAMPLES_PATH = "C:/Users/cheryl/Downloads/winnow-samples1"; // <-- use the path where you put the sample files.
+    private static final int FEATURE_LENGTH = 5; // Tokens per window.
+    
+    private static final double THICKNESS_THRESHOLD = 0.05; // Winnow thickness threshold.
+    
+    private TreeMap<String, cFeature> mDatabase = null;
+    
+    private int mFeaturesCreated = 0;
+    
+    public ScoreGrayCalculation()
+    {
+        this.mDatabase = new TreeMap<String, cFeature>();
+        this.processSamples();
+        return;
+    }
+    
+    private void processSamples()
+    {
+        File dir = new File(SAMPLES_PATH);
+        String[] filesFound = this.randomizeListOrder(dir.list());
+        
+        for(String aFile : filesFound)
+        {
+            if(aFile.endsWith("spam") || aFile.endsWith("good")){
+                
+                String text = this.extractTextFromFile(SAMPLES_PATH + "\\" + aFile);
+                ArrayList<String> tokens = this.tokenize(text);
+                // Arbitrarily designating "spam" and "good" in the file name facilitates the supervised learning.
+                String sampleType = aFile.substring(0, 4);
+                
+                if(sampleType.compareToIgnoreCase("spam") == 0){
+                    this.test(tokens, true);
+                }else{
+                    this.test(tokens, false);
+                }
+                this.train(aFile);
+            }
+        }
+        return;
+    }
+    
+    public int test(final ArrayList<String> tokens, final boolean isSpam)
+    {
+        TreeMap<String, cFeature> featureMap = this.createFeatureMap(tokens);
+        ArrayList<String> activeFeatures = this.getActiveFeatures(featureMap);
+        
+        if(isSpam){
+            int detect = this.calculateWeights(true, activeFeatures);
+            if(detect == 0)
+                return 2;
+            else
+                return 1;
+        }else{
+            this.calculateWeights(false, activeFeatures);
+            return 0;
+        }
+        
+        
+    }
+    
+    public void train(final String fileName)
+    {
+        String text = this.extractTextFromFile(SAMPLES_PATH + "\\" + fileName);
+        ArrayList<String> tokens = this.tokenize(text);
+        this.storeFeatures(tokens);
+        
+        return;
+    }
+    
+    //In this example, the weights of all active features are subject to adjustment, not just the ones that are equal to 1.0.
+    //This means that features can have their weights repeatedly adjusted, and to virtually any size.
+    private int calculateWeights(final boolean isSpam, ArrayList<String> activeFeatures)
+    {
+        double goodSum = 0.0;
+        double spamSum = 0.0;
+        int totalFeatures = activeFeatures.size();
+        cFeature feature = null;
+        
+        for(int i = 0; i < totalFeatures; i++)
+        {
+            feature = this.mDatabase.get(activeFeatures.get(i));
+            goodSum += feature.getGoodWeight(); // sum the good weights.
+            spamSum += feature.getSpamWeight(); // sum the spam weights.
+        }
+        
+        
+        double rGood = (2*goodSum)/(goodSum+spamSum);
+        double rBad = (2*spamSum)/(goodSum+spamSum);
+        
+        if(rBad+rGood>0){
+            double pSpam = spamSum/(goodSum+spamSum);
+            if(pSpam<0.01)
+                pSpam = 0.01;
+            else if(pSpam>0.99)
+                pSpam = 0.99;
+            
+            double pX = 1/(1+(1-pSpam));
+            double gX = 0.5 - Math.abs(pX-0.5);
+            if(gX<0.2){
+                return 0;
+            }
+            else{
+                return 1;
+            }   
+        }
+                
+        return 0;
+    }
+    
+       
+    private ArrayList<String> getActiveFeatures(TreeMap<String, cFeature> featureMap)
+    {
+        ArrayList<String> activeFeatures = new ArrayList<String>();
+        
+        for(String key : featureMap.keySet())
+        {
+            if(this.mDatabase.containsKey(key)){
+                activeFeatures.add(key);
+            }
+        }
+        
+        return activeFeatures;
+    }
+    
+    private void storeFeatures(final ArrayList<String> tokens)
+    {
+        boolean done = false;
+        int index = FEATURE_LENGTH - 1;
+        cFeature feature = null;
+        
+        while(!done)
+        {
+            if(index <= tokens.size()){
+                for(int i = 1; i < FEATURE_LENGTH; i++)
+                {
+                    feature = new cFeature();
+                    feature.setData(FEATURE_LENGTH - 1, tokens.get(index));
+                    feature.setData((FEATURE_LENGTH - 1) - i, tokens.get(index - i));
+                    
+                    // Each key represents the feature data concatenated together into one string.
+                    String key = "";
+                    for(int j = 0; j < FEATURE_LENGTH; j++)
+                    {
+                        if(feature.getData(j) == ""){
+                            key += " ";
+                        }else{
+                            key += feature.getData(j);
+                        }
+                    }
+                    
+                    if(!this.mDatabase.containsKey(key)){
+                        this.mDatabase.put(key, feature);
+                        this.mFeaturesCreated++;
+                    }
+                }
+            }
+
+            index += 1;
+            if(index > tokens.size() - 1){
+                done = true;
+            }
+        }
+        return;
+    }
+    
+    public TreeMap<String, cFeature> createFeatureMap(ArrayList<String> tokens)
+    {
+        boolean done = false;
+        int index = FEATURE_LENGTH - 1;
+        TreeMap<String, cFeature> featureMap = new TreeMap<String, cFeature>();
+        cFeature feature = null;
+
+        while(!done)
+        {
+            if(index <= tokens.size()){
+                for(int i = 1; i < FEATURE_LENGTH; i++)
+                {
+                    feature = new cFeature();
+                    feature.setData(FEATURE_LENGTH - 1, tokens.get(index));
+                    feature.setData((FEATURE_LENGTH - 1) - i, tokens.get(index - i));
+                    
+                    // Each key represents the feature data concatenated together into one string.
+                    String key = "";
+                    for(int j = 0; j < FEATURE_LENGTH; j++)
+                    {
+                        if(feature.getData(j) == ""){
+                            key += " ";
+                        }else{
+                            key += feature.getData(j);
+                        }
+                    }
+                    
+                    if(!featureMap.containsKey(key)){
+                        featureMap.put(key, feature);
+                    }
+                }
+            }
+
+            index += 1;
+            if(index > tokens.size() - 1){
+                done = true;
+            }
+        }
+        return featureMap;
+    }
+    
+    public ArrayList<String> tokenize(final String document)
+    {
+        // Plain: everything except whitespace and control characters.
+        //           [^\p{Z}\p{C}]+
+        // CRM114: start with a printable character; followed by any number of
+        //         alphanumeric characters + dashes, dots, commas and colons;
+        //         optionally ended by a printable character.
+        //           [^\p{Z}\p{C}][-.,:\p{L}\p{M}\p{N}]*[^\p{Z}\p{C}]?
+        // Simplified CRM114: excludes dots, commas and colons from the middle
+        //         of the pattern. With this pattern, domain names and mail addresses
+        //         will be split at dots, so the classifier can recognize a domain
+        //         even if subdomains vary.
+        //           [^\p{Z}\p{C}][-\p{L}\p{M}\p{N}]*[^\p{Z}\p{C}]?
+        
+        Pattern pattern = Pattern.compile("[\\p{Cntrl}\\p{Space}]"); // Java has it's own regex specs.
+        
+        ArrayList<String> tokens = new ArrayList<String>();
+
+        String items[] = pattern.split(document);
+        for(String item : items)
+        {
+            if(item != null && !item.isEmpty()){
+                tokens.add(item);
+            }
+        }
+        return tokens;
+    }
+    
+    private String extractTextFromFile(final String filePath)
+    {
+        BufferedReader reader = null;
+        StringBuffer buffer = null;
+        try{
+            reader = new BufferedReader(new FileReader(filePath));
+            
+            buffer = this.fillStringBuffer(reader);
+            
+            reader.close();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        return buffer.toString();
+    }
+    
+    private StringBuffer fillStringBuffer(BufferedReader reader)
+    {
+        StringBuffer buffer = new StringBuffer();
+        boolean done = false;
+        while(!done){
+            try{
+                String line = reader.readLine();
+                if(line != null){
+                    buffer.append(line);
+                    buffer.append("\n");
+                }else{
+                    done = true;
+                }
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+                done = true;
+            }
+        }
+        return buffer;
+    }
+    
+    private String[] randomizeListOrder(String[] aList)
+    {
+        Random rand = new Random();
+        int listSize = aList.length;
+        int shuffleCount = 0;
+        boolean done = false;
+        
+        while(!done)
+        {
+            int index1 = rand.nextInt(listSize);
+            int index2 = rand.nextInt(listSize);
+            if(index1 == index2){ continue; } // The random numbers need to differ.
+            
+            String temp = aList[index1];
+            aList[index1] = aList[index2];
+            aList[index2] = temp;
+            
+            shuffleCount++;
+            if(shuffleCount >= listSize){
+                done = true;
+            }
+        }
+        
+        return aList;
+    }
+
+    
+    
+    private class cFeature
+    {
+        // A null or empty string in an mData element signifies a "skip".
+        private String mData[] = new String[FEATURE_LENGTH];
+        
+        
+        double mSpamWeight = 1.0;
+        double mGoodWeight = 1.0;
+    
+        public cFeature() {    return;    }
+    
+        public void setSpamWeight(double weightValue)
+        {
+            this.mSpamWeight = weightValue;
+            return;
+        }
+        
+        public double getSpamWeight()
+        {
+            return this.mSpamWeight;
+        }
+        
+        public void setGoodWeight(double weightValue)
+        {
+            this.mGoodWeight = weightValue;
+            return;
+        }
+        
+        public double getGoodWeight()
+        {
+            return this.mGoodWeight;
+        }
+    
+        public void setData(final int index, final String token)
+        {
+            this.mData[index] = token;
+            return;
+        }
+        
+        public String getData(int index)
+        {
+            return this.mData[index];
+        }
+
+    } // end cFeature
+    
+    public static void main(String[] args) 
+    {
+        ScoreGrayCalculation winnow1 = new ScoreGrayCalculation();
+        return;
+    }
+    
+}
